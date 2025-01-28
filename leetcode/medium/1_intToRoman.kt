54b10a4af12990cab11d4ce21ea4e680dd97e589
HanhVoS3Corp <hanh.vo@s3corp.com.vn>
2025-01-28 23:24:05 +0700

    leetcode: medium - add better solution for problem int to roman

--- leetcode/medium/1_intToRoman.kt
+++ leetcode/medium/1_intToRoman.kt
@@ -1,4 +1,37 @@
-fun intToRoman(num: Int): String {
+// Solution 1: takes 1 ms
+val symbols = arrayOf(
+    1000 to "M",
+    900 to "CM",
+    500 to "D",
+    400 to "CD",
+    100 to "C",
+    90 to "XC",
+    50 to "L",
+    40 to "XL",
+    10 to "X",
+    9 to "IX",
+    5 to "V",
+    4 to "IV",
+    1 to "I",
+)
+
+// it reduces 1 ms
+@JvmField
+val numerals = Array(4000) { convert(it) }
+
+fun convert(num: Int): String = buildString {
+    symbols.fold(num) { acc, (n, str) ->
+        val times = acc / n
+        // it reduces time
+        for (i in 0..times) append(str)
+        acc - n * times
+    }
+}
+
+inline fun Any?.intToRoman(num: Int): String = numerals[num]
+
+// Solution 2: takes 3 ms
+fun intToRoman2(num: Int): String {
     var result = StringBuilder()
     var t = num
     while (t > 0) {
@@ -70,6 +103,6 @@ fun intToRoman(num: Int): String {
 }
 
 fun main() {
-  val romanNumeral = intToRoman(58)
+  val romanNumeral = intToRoman2(58)
   println(romanNumeral)
 }
