c3ee217713e86d9057718be9445f6a23a078a36c
vnhanh <nghiamy15@gmail.com>
2025-03-26 23:30:59 +0700

    leetcode: medium-letter combinations a phone number

---
+++ leetcode/medium/10_17_letter_combinations_a_phone_number.kt
@@ -0,0 +1,41 @@
+/**
+ * Solution 1: 4ms. Brute force
+ */
+fun letterCombinations4(digits: String): List<String> {
+  val map = hashMapOf<Char,String>(
+      '2' to "abc",
+      '3' to "def",
+      '4' to "ghi",
+      '5' to "jkl",
+      '6' to "mno",
+      '7' to "pqrs",
+      '8' to "tuv",
+      '9' to "wxyz"        
+  )
+  val res: ArrayList<String> = arrayListOf()
+  val t: ArrayList<String> = arrayListOf()
+  for (d in digits) {
+      val chars: String? = map[d]
+      if (chars == null) continue
+      if (res.isEmpty()) {
+          for (c in chars) {
+              t.add(c.toString())
+          }
+      } else {
+          for (str in res) {
+              for (c in chars) {
+                  t.add("$str${c.toString()}")
+              }
+          }
+      }
+
+      res.clear()
+      res.addAll(t)
+      t.clear()
+  }
+  return res
+}
+
+fun main() {
+
+}
